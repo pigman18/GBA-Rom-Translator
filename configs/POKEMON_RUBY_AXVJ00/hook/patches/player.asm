abe620b01681289e58083fbecb1fa9af92325814
ZhangJiaHao <zhangjh@huatugz.com>
2026-08-27 19:07:48 +0800

    feat: ruby
    1、玩家初始名

--- configs/POKEMON_RUBY_AXVJ00/hook/patches/player.asm
+++ configs/POKEMON_RUBY_AXVJ00/hook/patches/player.asm
@@ -1,15 +1,12 @@
 .loadtable "./patches/unit_charmap.txt"
 
-.org 0x081DA780
-.align 2
-Brendan:
-    .strn "ユウキ$"
-May:
-    .strn "ハルカ$"
-
-
-; --- 修改初始名称为 佑树、小遥 ---
+; 起名菜单 preset #1：内联 PCS（指针表 0x081BC1CC 男 / 0x081BC1F4 女 指入此处）
+; 禁止 .word 外链指针；slot 运行时：ユウキ→祐树、ハルカ→小遥
+; 与 0x083E944C/9450 训练家名表字节一致，便于串档
 .org 0x083E9640
-    .word   Brendan
+    .strn "ユウキ$"
+    .byte   0xFF, 0xFF
+
 .org 0x083E9658
-    .word   May
+    .strn "ハルカ$"
+    .byte   0xFF, 0xFF
